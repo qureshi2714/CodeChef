c21efb63b954f658eb5e83f08e1b54623c5c9f2b
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-06 21:07:43 +0530

    Add files via upload

---
+++ Christmas.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Christmas
+{
+    public static void main(String args[]) throws java.lang.Exception
+    {
+     //your code goes here
+         Scanner sc = new Scanner(System.in);
+    int d=sc.nextInt();
+    if(d==25)
+    System.out.println("CHRISTMAS");
+    else
+    System.out.println("ORDINARY");
+    }
+}            
