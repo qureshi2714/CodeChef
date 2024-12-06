864e4cb7e60499f43c5e831555926277d2262ba9
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-06 20:50:31 +0530

    Add files via upload

---
+++ Luckyseven.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Luckyseven
+{
+    public  static void main(String args[]) throws java.lang.Exception
+    {
+    Scanner sc = new Scanner(System.in);
+    String S=sc.next();
+    System.out.println(S.charAt(6));
+    }
+}    
+        
+    
+
