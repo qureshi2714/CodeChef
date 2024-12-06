9a73cf6dfb9758f7280bcb47caf73a523e2048a3
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-06 20:59:36 +0530

    Add files via upload

---
+++ Start1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Start1
+{
+    public static void main(String args[])
+    {
+        //Do not print here anything before taking the input
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //print the value of n
+        System.out.println(n);
+    }
+}        
