71ba8006a080c7f0031cb67bbc628b8d4d25bae2
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-06 21:16:23 +0530

    Add files via upload

---
+++ AIcoming.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class AIcoming
+{
+    public static void main(String args[]) throws java.lang.Exception
+    {
+        // your code goes here
+    Scanner sc = new Scanner (System.in);
+    int n=sc.nextInt();
+    if(n<=60)
+    System.out.println("YES");
+    else
+    System.out.println("NO");
+    }
+}    
