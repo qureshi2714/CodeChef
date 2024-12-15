fdd25ac3b7db6f6a42cc917563812cf7c47afdc5
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:49:48 +0530

    Add files via upload

---
+++ Offby1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Offby1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		int c=A+B;
+		int d=1;
+		System.out.println(c+""+d);
+
+	}
+}
