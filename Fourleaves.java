3b20307676da1c725f14fae5d44b716fc864b272
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:38:11 +0530

    Add files via upload

---
+++ Fourleaves.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Fourleaves{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        System.out.println(((4)+(3*(N-1))));
+    }
+}
