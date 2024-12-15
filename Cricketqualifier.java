a9455f3ca2c5c158384fc85a802ad0db02c59be5
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:32:07 +0530

    Add files via upload

---
+++ Cricketqualifier.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Cricketqualifier
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x>=12)
+		{
+		    System.out.println("YES");
+		}
+		else
+		{
+		    System.out.println("No");
+		}
+
+	}
+}
