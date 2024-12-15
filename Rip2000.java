39b3aaa946bd86e436a24e3291e0c29f738cb6dd
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:54:11 +0530

    Add files via upload

---
+++ Rip2000.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Rip2000
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+       Scanner sc=new Scanner(System.in);
+       int N=sc.nextInt();
+       int c=4*N;
+       System.out.println(c);
+
+	}
+}
