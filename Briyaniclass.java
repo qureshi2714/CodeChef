d59d711846a3e9ca0c511a9bb951c6d19566bb7d
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:29:05 +0530

    Add files via upload

---
+++ Briyaniclass.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Biryaniclass
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    while(t-->0)
+	    {
+	        int X=sc.nextInt();
+	        int Y=sc.nextInt();
+	        int z=X*Y;
+	        System.out.println(z);
+	    }
+
+	}
+}
