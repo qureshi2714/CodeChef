ac87bad9517e3868fe2888ca49b5417bc7575fe9
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:45:08 +0530

    Add files via upload

---
+++ Ioi2024.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Ioi2024{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        if(X<=8){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
