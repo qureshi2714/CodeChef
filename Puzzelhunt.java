3ad3d639bafbe871975a8f793aa6239b35ae30b5
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:52:28 +0530

    Add files via upload

---
+++ Puzzelhunt.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Puzzelhunt
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int N=sc.nextInt();
+	if(N>=6 && N<=8)
+	{
+	    System.out.println("YES");
+	}
+	else{
+	    System.out.println("NO");
+	}
+
+	}
+}
