3e66b269ee50df17ca69df983a241bcab64b5280
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:45:58 +0530

    Add files via upload

---
+++ Justiceday.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Justiceday
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(X>=Y)
+		{
+		    System.out.println("YES");
+		}
+		else
+		{
+		    System.out.println("NO");
+		}
+
+	}
+}
