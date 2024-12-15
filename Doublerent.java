1d26e8afeb1c8bffb05c134bd5c7ec9ddb4c54f4
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:35:41 +0530

    Add files via upload

---
+++ Doublerent.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Doublerent{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        System.out.println(X*2);
+    }
+}
