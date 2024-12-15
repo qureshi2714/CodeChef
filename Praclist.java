5ad86aac4c49a0ec4accaae9df08a880334fd152
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:51:56 +0530

    Add files via upload

---
+++ Praclist.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Praclist
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		int c=X-Y;
+		System.out.println(c);
+
+	}
+}
