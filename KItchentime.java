49d79f76096092b81ff6a33a431ef88da6ff2d7b
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:46:26 +0530

    Add files via upload

---
+++ KItchentime.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+class Kitchentime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int t=sc.nextInt();
+	 for(int i=0;i<t;i++)
+	 {
+	 int X=sc.nextInt();
+	 int Y=sc.nextInt();
+	 int c=Y-X;
+	 System.out.println(c);
+
+	}
+}
+}
