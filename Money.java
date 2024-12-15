7853bb52472d750120d408228c3ce2ced3964384
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:48:05 +0530

    Add files via upload

---
+++ Money.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Money{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=x*5000+y*9800;
+        System.out.println("the amount of money chef can earn by selling items"+z);
+        }
+}
