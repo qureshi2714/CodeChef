1a80d601be6eb0ea6b4026d52ab9086323615947
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:50:18 +0530

    Add files via upload

---
+++ Overspeeding.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Overspeeding
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int S=sc.nextInt();
+		if(S<=40){
+		    System.out.println("NO");
+		}
+		else{
+		    System.out.println("YES");
+		}
+	}
+}
