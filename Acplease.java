c5a58b2ef8e836de1fb7275898063e0751cd3e37
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:26:55 +0530

    Add files via upload

---
+++ Acplease.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Acplease
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		if(T>30)
+		{
+		    System.out.println("YES");
+		}
+		else
+		{
+		    System.out.println("NO");
+		}
+
+	}
+}
