ba9000e8691913d008d5307db529047dbb48a3f8
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:56:38 +0530

    Add files via upload

---
+++ Taxsaving.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Taxsaving
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int t=sc.nextInt();
+	  for(int i=0;i<t;i++)
+	  {
+	      int X=sc.nextInt();
+	      int Y=sc.nextInt();
+	      int c=X-Y;
+	      System.out.println(c);
+	  }
+
+	}
+}
