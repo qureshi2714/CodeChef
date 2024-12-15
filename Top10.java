57a8a056d3f7c48483e6435dfb0e874ec975ef55
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:58:18 +0530

    Add files via upload

---
+++ Top10.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Top10
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int X=sc.nextInt();
+		    if(X<=10)
+		    {
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
