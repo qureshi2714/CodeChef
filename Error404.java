c29ed875a8620c466537afcb92997ef6171c8501
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:36:10 +0530

    Add files via upload

---
+++ Error404.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Error404
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int X=sc.nextInt();
+	if(X==404)
+	{
+	    System.out.println("NOT FOUND");
+	}
+	else{
+	    System.out.println("FOUND");
+	}
+
+	}
+}
