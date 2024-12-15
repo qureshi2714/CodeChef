80c919d4c29a99b4b260b80e8126b75e50d0f088
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:41:55 +0530

    Add files via upload

---
+++ Goldcoins101.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Goldcoins101
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(X>Y)
+		{
+		    System.out.println(A);
+		}
+		else{
+		    System.out.println(B);
+		}
+
+	}
+}
