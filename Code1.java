6aeaf2e30351a0931a5029b583b6f9111a2d3e46
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:31:06 +0530

    Add files via upload

---
+++ Code1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Code1{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=6*x+y;
+        System.out.println("the weekly cost of food is:"+z);
+    }
+}
