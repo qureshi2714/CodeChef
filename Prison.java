e42911681f213bed0c2fbba66492c758b2f96c6e
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:53:17 +0530

    Add files via upload

---
+++ Prison.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Prison
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int X=sc.nextInt();
+	 if(X>=7)
+	 {
+	     System.out.println("YES");
+	 }
+      else
+      {
+          System.out.println("No");
+      }
+	}
+}
