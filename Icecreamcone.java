26448bebf29511f066d1c328c59702265eac8e6d
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:43:31 +0530

    Add files via upload

---
+++ Icecreamcone.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Icecreamcone
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(X==Y)
+		{
+		    System.out.println(X);
+		}
+		else{
+		    System.out.println(X-Y);
+		}
+
+	}
+}
