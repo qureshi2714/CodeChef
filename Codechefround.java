5e6d00f169193339a41640083f26d2643806e9b3
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:31:46 +0530

    Add files via upload

---
+++ Codechefround.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Codechefround{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==4)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
