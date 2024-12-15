3a730e9daa5f0e561c0a3de158122d2afc536791
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:59:43 +0530

    Add files via upload

---
+++ Yogaday.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Yogaday
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int N=sc.nextInt();
+	System.out.println(N/12);
+
+	}
+}
