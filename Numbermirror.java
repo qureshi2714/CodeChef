60a903a250e47d9fc7676a3e7687fc0e56b021f6
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:49:18 +0530

    Add files via upload

---
+++ Numbermirror.java
@@ -0,0 +1,11 @@
+import java.util.*; 
+ public class Numbermirror
+{
+	public static void main (String[] args)
+	{
+	    // Do not print anything here before taking input
+	    Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(a);
+	}
+}
