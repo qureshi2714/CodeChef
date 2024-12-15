a34c936370ea2f6f2ef12c6a39340ec814d055df
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:44:11 +0530

    Add files via upload

---
+++ Idesofmarch.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Idesofmarch
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n==15){
+		    System.out.println("yes");
+		}
+		else{
+		    System.out.println("no");
+		}
+	}
+}
