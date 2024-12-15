afcb22ea04186cb7a0901b1315f90060bdc007e6
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:30:36 +0530

    Add files via upload

---
+++ Clearday.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Clearday
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		int c=X+Y;
+		System.out.println((-c)+7);
+		
+
+	}
+}
