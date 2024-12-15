a6a23ea93d0e6c19421b60607b400c1381c8c45d
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:48:49 +0530

    Add files via upload

---
+++ Newssports.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Newssports
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+     Scanner sc=new Scanner(System.in);
+     int X=sc.nextInt();
+      if(X>=8)
+      {
+          System.out.println("YES");
+      }
+      else{
+          System.out.println("NO");
+      }
+
+	}
+}
