3542299d89f71b9c51bbc5d3e4fcb5c7a3bdd4b8
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:34:33 +0530

    Add files via upload

---
+++ Donut.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ class Donut
+{
+	public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		System.out.println(X*Y);
+
+	}
+}
