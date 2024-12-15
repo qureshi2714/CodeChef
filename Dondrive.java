c3055139de7ed55b3778f62f219efe3fbcb48b94
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:34:11 +0530

    Add files via upload

---
+++ Dondrive.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Dondrive
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for( int i=0;i<t;i++)
+		{
+		    int N=sc.nextInt();
+		    int X=sc.nextInt();
+		    int c=N-X;
+		    System.out.println(c);
+		}
+
+	}
+}
