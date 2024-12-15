318dc89a9f0e4b09b51f004ff5b837359bd70ca6
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-15 20:51:26 +0530

    Add files via upload

---
+++ Parity.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Parity
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int N=sc.nextInt();
+		    if(N%2==0){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
